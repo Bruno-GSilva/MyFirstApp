71833afe4f52feeebb286cf573a9496220346a5d
Bruno-GSilva <brunogomesdsilva8@gmail.com>
2023-04-16 12:53:13 -0300

    updateData

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,24 +1,22 @@
 import React from "react";
 import { Image, Pressable, Text, View } from "react-native";
 
-function Card() {
+export default function Card(props) {
   return (
     <View className="w-auto h-48 flex-row px-4 py-6 my-2 mx-2 items-center border-2 rounded-xl bg-black shadow-2xl shadow-white border-yellow-500 overflow-hidden">
       <Pressable className="h-full w-32 mr-6 border-2 rounded-md border-white overflow-hidden active:border-yellow-500">
         <Image
           source={{
-            uri: "https://www.crunchyroll.com/imgsrv/display/thumbnail/480x720/catalog/crunchyroll/251524e3b5517b689317437d881eccf0.jpe",
+            uri:`${!props.url? "https://avatars.githubusercontent.com/u/40152459?v=4": props.url}`,
           }}
           className="flex-1"
         />
-        <Text className="text-center bg-amber-500">Nome do Anime</Text>
+        <Text className="text-center bg-amber-500">{!props.name? "Nome do Anime": props.name}</Text>
       </Pressable>
       <View className="w-48 overflow-y-hidden">
-        <Text className='text-base font-semibold text-white'>Descrição do Anime</Text>
-        <Text className='text-sm text-slate-500'>loremsksdkjakjsdhkahsdkhaksjhdkjahskjdhkjahskdjhakjshdjkhajkshdkjahjsdhkajhsjdkhakjhsdkjhajkshdkjashdjkhasdalksjdlkajs...</Text>
+        <Text className='text-base font-semibold text-white'>{!props.titule? "Descrição do Anime": props.titule}</Text>
+        <Text className='text-sm text-slate-500'>{!props.description? "Substitute Soul Reaper Ichigo Kurosaki spends his days fighting against Hollows...": props.description}</Text>
       </View>
     </View>
   );
 }
-
-export default Card;
--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,22 +1,66 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { ScrollView, Text, View } from "react-native";
 import Header from "../components/Header";
 import Card from "../components/Card";
 import Button from "../components/Button";
 
 export default function HomeScreen() {
+  // const [dataCards, setDataCards] = useState(Data);
+
+  const Data = [
+    {
+      id: "01",
+      name: "Boku No Hero",
+      titule:"My Hero Academia",
+      description:
+        "Por toda a sua vida, Izuku sonhou ser um heroi — um objetivo ambicioso para qualquer um, mas especialmente desafiador para um garoto sem superpoderes. Isso mesmo: em um mundo onde 80% da população tem algum tipo de Dom especial, Izuku teve a má sorte de nascer completamente normal. Mas isso não vai impedi-lo de se matricular em uma das academias de herois mais prestigiosas do mundo",
+      episode: "138",
+      image:
+        "https://www.crunchyroll.com/imgsrv/display/thumbnail/1200x675/catalog/crunchyroll/aa31911c746c4d7d027b190f2e184834.jpe",
+    },
+    {
+      id: "02",
+      name: "BOFURI",
+      titule:"BOFURI",
+      description:
+        "Maple quer evitar se machucar em seu jogo de VRMMO, então ela coloca todos os seus pontos de habilidade na defesa até não conseguir nem mesmo se movimentar. E funciona??",
+      episode: "23",
+      image:
+        "https://www.crunchyroll.com/imgsrv/display/thumbnail/1200x675/catalog/crunchyroll/2583f9f13ed1f26108ae4279f5b50c94.jpe",
+    },
+    {
+      id: "03",
+      name: "BORUTO",
+      titule:"BORUTO",
+      description:
+        "Uzumaki Boruto, filho de Uzumaki Naruto, o Sétimo Hokage, se inscreveu na Academia Ninja para aprender a ser um verdadeiro ninja. Os outros estudantes o ignoram por ser 'apenas o filho do Hokage', mas a paixão e a personalidade do Boruto vai acabar com todos esses preconceitos. Quando uma série de misteriosos eventos começa a se desenrolar, cabe a Boruto e seus novos amigos a investigá-los. Como um tornado, Boruto entra no coração de todos: sua história está prestes a começar!",
+      episode: "293",
+      image:
+        "https://www.crunchyroll.com/imgsrv/display/thumbnail/1200x675/catalog/crunchyroll/bdbd4f4e247f7290eb847025021e133b.jpe",
+    },
+    {
+      id: "04",
+      name: "Demon Slayer",
+      titule:"Kimetsu no Yaiba",
+      description:
+        "Japão, era Taisho. Tanjiro, um bondoso jovem que ganha a vida vendendo carvão, descobre que sua família foi massacrada por um demônio. E pra piorar, Nezuko, sua irmã mais nova e única sobrevivente, também foi transformada num demônio. Arrasado com esta sombria realidade, Tanjiro decide se tornar um matador de demônios para fazer sua irmã voltar a ser humana, e para matar o demônio que matou sua família. Um triste conto sobre dois irmãos, onde os destinos dos humanos e dos demônios se entrelaçam, começa agora.",
+      episode: "125",
+      image:
+        "https://www.crunchyroll.com/imgsrv/display/thumbnail/1200x675/catalog/crunchyroll/f7adcedd1d7c53ae18d851003a3cfae4.jpe",
+    },
+  ];
+
   return (
     <View className="flex-1 items-center bg-black">
       <Header />
-      <View className='w-full flex-row items-center justify-around'>
+      
+      <View className="w-full flex-row items-center justify-around">
         <Text className="text-2xl font-bold text-white my-8">Minha Lista</Text>
-        <Button text={'Adicionar'}/>
+        <Button text={"Adicionar"} />
       </View>
       <ScrollView>
-        <Card />
-        <Card />
-        <Card />
-        <Card />
+        <Card/>
+        {Data.map( e => <Card url={e.image} titule={e.titule} description={e.description} name={e.name}/>)}
       </ScrollView>
     </View>
   );
